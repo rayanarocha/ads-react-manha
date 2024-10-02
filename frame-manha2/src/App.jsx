7e796bb55a0beb770b27b29ebb73d7f50810dd29
rayanarocha <rayana.azus@gmail.com>
2024-10-02 09:53:10 -0300

    manipulando imagens da pasta src e da pasta public, trabalhando com JSX fora do return

---
+++ frame-manha2/src/App.jsx
@@ -0,0 +1,28 @@
+import './App.css'
+import HalfDakotaPiata from "./assets/halfdkttonton.jpg"
+
+// const renderComponent = (x) => {
+//   if(x) {
+//     return <h1>Renderizou dentro do if</h1>
+//   } else {
+//     return <h1>Renderizou dentro do else</h1>
+//   }
+// }
+
+function App() {
+
+  return (
+    <div>
+      <div>
+            <div>
+                <img src="./public/dkttonton.jpg" alt="Foto de Dakota e Piatã boca de tubarão" width='250' height='200' />
+            </div>
+            <div>
+                <img src={HalfDakotaPiata} alt="Half part of Dakota e Piatã" width='250' height='200'/>
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default App
